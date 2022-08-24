5b4eb14fc274ae9469749b6869e799305e2e90fe
FRANCISCO BATISTA CAVALCANTI NETO <n.cavalcanti@academico.ifrn.edu.br>
2022-08-24 14:11:27 -0300

    colocando os arq da minha aplicação

---
+++ src/main/java/neto/pi/demo/controllers/indexController.java
@@ -0,0 +1,13 @@
+package neto.pi.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+	@RequestMapping("/")
+	public String index(){
+		System.out.println("chamou metodo index");
+		return "home";
+	}
+}
